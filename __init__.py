315612ecc82ee5c5858da53be00e15d1bc5e785b
chris <chris.goringe@gmail.com>
2024-12-17 09:51:50 +1100

    rewid

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .noise_nodes import MixNoise, ShapeNoise
 from .noise_batch_nodes import SeperableBatchNoise, BatchNoiseSimulate
+from .renoise_calculation import SplitSigmasWithRewind
 
 VERSION = "1.1"
 
@@ -8,5 +9,6 @@ NODE_CLASS_MAPPINGS = {
     "Shape Noise"           : ShapeNoise,
     "Seperable Batch Noise" : SeperableBatchNoise,
     "Batch Noise Simulate"  : BatchNoiseSimulate,
+    "Split Sigmas with Rewind" : SplitSigmasWithRewind,
 }
 __all__ = ['NODE_CLASS_MAPPINGS',]
